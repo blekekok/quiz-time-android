43bae192dd90ee697d8a4105db0636f931fb1e42
JeanArct <jeanelin007@gmail.com>
2024-05-22 23:02:40 -0700

    0.0.2 - adding buttons

--- app/src/main/java/net/atlas/projectalpha/MainActivity.java
+++ app/src/main/java/net/atlas/projectalpha/MainActivity.java
@@ -2,9 +2,14 @@ package net.atlas.projectalpha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import net.atlas.projectalpha.databinding.ActivityMainBinding;
@@ -17,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+    private ImageView ivLogo;
+    private Button btnSignIn, btnSignUp;
+    private EditText edtSearch;
+//    private ListView lvQuizList = findViewById(R.id.lvQuizList);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +34,46 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Logo
+        ivLogo = findViewById(R.id.ivLogo);
+        ivLogo.setImageResource(R.drawable.ic_launcher_background);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        edtSearch = findViewById(R.id.edtSearch);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SignInActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SignUpActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
 //        // Example of a call to a native method
 //        TextView tv = binding.sampleText;
 //        tv.setText(stringFromJNI());
 
-        LinearLayout quizList = findViewById(R.id.quizList);
-
-        for (int i = 0; i < 5; i++) {
+        /*for (int i = 0; i < 5; i++) {
             View itemView = getLayoutInflater().inflate(R.layout.quiz_item_layout, null);
 
             TextView quizTitle = itemView.findViewById(R.id.titleText);
             quizTitle.setText("TEST" + i);
 
             quizList.addView(itemView);
-        }
+        }*/
     }
 
     /**
---
+++ app/src/main/java/net/atlas/projectalpha/SignUpActivity.java
@@ -0,0 +1,8 @@
+package net.atlas.projectalpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+}
